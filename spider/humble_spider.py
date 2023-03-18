02d9b75ea55fd27a269e45b536e2022bce2d0fd4
dopeldev <322kuroneko@gmail.com>
2023-03-18 16:05:00 -0500

    add libro spider todo

--- spider/humble_spider.py
+++ spider/humble_spider.py
@@ -8,47 +8,54 @@ from json import loads
 """
     obtiene el tag de java_script_content 
     remueve el tag con remove_tag
-    retorna un Objeto json
+    retorna un Objeto json serializado
 """
 
+class HumbleSpider:
 
+    def __init__(self):
+        self.URL = 'https://www.humbleBundle.com/books'
+        self.json_object = self.get_json()
+        self.content = self.get_content(self.json_object)
 
-def get_json() -> Dict:
+    def get_json(self) -> Dict:
+        try:
+            response = get(self.URL)
+            response.raise_for_status()
+        except exceptions.RequestException as e:
+            print(f'Error no se obtuvo respuesta 200 GET : {e}')
+            sys.exit()
 
-    URL = 'https://www.humbleBundle.com/books'
+        sopa = BeautifulSoup(response.text, 'html.parser')
+        java_script_content = sopa.find_all('script', {'id':{'landingPage-json-data'}})
+        clean_data = self.remove_tag(java_script_content)
+        return loads(clean_data) 
 
-    try:
-        response = get(URL)
-        response.raise_for_status()
-    except exceptions.RequestException as e:
-        print(f'Error no se obtuvo respuesta 200 GET : {e}')
-        sys.exit()
+    def remove_tag(self, java_script_content):
+        dirty_text = str(java_script_content)
+        cutString : str = ''
+        count = 0
+        for item in dirty_text:
+            if item == '>':
+                cutString += item
+                count += 1
+                break
+            else:
+                cutString += item
+                count += 1
+        # 10 chars dub </scripts>
+        clean_text = dirty_text[count:-10]
+        return clean_text 
 
-    sopa = BeautifulSoup(response.text, 'html.parser')
-    java_script_content = sopa.find_all('script', {'id':{'landingPage-json-data'}})
-    clean_data = remove_tag(java_script_content)
-    return loads(clean_data) 
+    def get_content(self, object_json : Dict) -> pd.DataFrame:
+        content = object_json.get('data').get('books').get('mosaic')[0].get('products')
+        return pd.json_normalize(content)
 
-def remove_tag(java_script_content):
-    dirty_text = str(java_script_content)
-    cutString : str = ''
-    count = 0
-    for item in dirty_text:
-        if item == '>':
-            cutString += item
-            count += 1
-            break
-        else:
-            cutString += item
-            count += 1
-    # 10 chars dub </scripts>
-    clean_text = dirty_text[count:-10]
-    return clean_text 
-
-def get_content(object_json : Dict) -> pd.DataFrame:
-    content = object_json.get('data').get('books').get('mosaic')[0].get('products')
-    return pd.json_normalize(content)
-
-def get_link_title_price_url(dataframe : pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame(dataframe, columns=['product_url', 'machine_name'])
-
+    def get_urls(self) -> list:
+        urls_second_part = self.content['product_url'].tolist()
+        urls_first_part = 'https://www.humblebundle.com'
+        urls = [str]
+        for url in urls_second_part:
+            url = urls_first_part + url
+            urls.append(url)
+        return urls
