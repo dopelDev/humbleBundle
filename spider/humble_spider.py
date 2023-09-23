899e21308a1d88a7fe3b7360d2ca3d63e1e1381a
dopeldev <322kuroneko@gmail.com>
2023-09-23 03:38:40 -0500

    works

--- spider/humble_spider.py
+++ spider/humble_spider.py
@@ -1,28 +1,9 @@
 import sys
 from requests import get, exceptions
 from bs4 import BeautifulSoup
-from typing import Dict, NamedTuple, List
+from typing import Dict
 import pandas as pd
 from json import loads
-from datetime import timedelta, date
-
-
-"""
-    Datatypes
-"""
-
-class Url(NamedTuple):
-    url: str
-    name: str
-class Bundle(NamedTuple):
-    name: str
-    price: float
-    description: str
-    url: str
-    books: list[str]
-    time_start: date
-    time_end: date
-    time_countdown: timedelta
 
 """
     create a spider to get the json object from humble bundle
@@ -55,7 +36,7 @@ class HumbleSpider:
         clean_data = self.remove_tag(java_script_content)
         return loads(clean_data) 
 
-    def remove_tag(self, java_script_content):
+    def remove_tag(self, java_script_content) -> str:
         dirty_text = str(java_script_content)
         cutString : str = ''
         count = 0
@@ -82,40 +63,9 @@ class HumbleSpider:
     def get_urls(self) -> list:
         urls_second_part = self.content['product_url'].tolist()
         urls_first_part = 'https://www.humblebundle.com'
-        urls = [str]
+        urls = []
         for url in urls_second_part:
             url = urls_first_part + url
             urls.append(url)
         return urls
 
-"""
-    create a new spider and get the bundles information
-    
-    class BundleSpider(): get a list of urls and get the information of each bundle
-    def get_raw_data_bundle(self, List[str]) -> tbd
-"""
-
-class BundleSpider():
-    
-        def __init__(self):
-            self.humble_spider = HumbleSpider()
-            self.urls = self.humble_spider.get_urls()
-            self.raw_data = self.get_raw_data_bundle(self.urls)
-    
-        def get_raw_data_bundle(self, urls : list) -> list:
-            raw_data = []
-            for url in urls:
-                try:
-                    response = get(url)
-                    response.raise_for_status()
-                except exceptions.RequestException as e:
-                    print(f'Error no se obtuvo respuesta 200 GET : {e}')
-                    sys.exit()
-                sopa = BeautifulSoup(response.text, 'html.parser')
-                java_script_content = sopa.find_all('script', {'id':{'landingPage-json-data'}})
-                clean_data = self.humble_spider.remove_tag(java_script_content)
-                raw_data.append(loads(clean_data))
-            return raw_data
-
-        def get_urls(self, urls:List[str]):
-            return self.urls
